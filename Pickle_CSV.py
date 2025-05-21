b132530c97f56f883a5b963a64c622a1c56e8929
Muhammad Uzair <uzair8710.muhammad@gmail.com>
2025-05-21 20:36:22 +0300

    Update Pickle_CSV.py

--- Pickle_CSV.py
+++ Pickle_CSV.py
@@ -1,15 +1,26 @@
 import pickle
+import pandas as pd
 
-# Load the pickle file
-with open("GA_0500_kN_individuals.pickle", "rb") as f:  # Change filename as needed
+# Load the pickle file (the optimum one)
+with open("GA_0050_kN_best_individual.pickle", "rb") as f: #change as needed
     data = pickle.load(f)
 
-import pandas as pd
+# Convert the dict to a DataFrame
+df = pd.DataFrame([data])  # wrap in list of one dict
+
+# Save to CSV
+df.to_csv("50_best.csv", index=False)
+
+print("Converted successfully!")
+
+# Load the pickle file (complete results)
+with open("GA_0050_kN_individuals.pickle", "rb") as f:  # Change filename as needed
+    data = pickle.load(f)
 
 # Convert list of dictionaries to DataFrame
 df = pd.DataFrame(data)
 
 # Save to CSV
-df.to_csv("output_500.csv", index=False)
+df.to_csv("output_50.csv", index=False)
 
 print("Pickle file converted to CSV successfully!")
